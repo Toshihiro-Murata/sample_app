1f8d355cbd809a270ee62f2a05ba380fcd218c83
Toshihiro Murata <toshihiro.n.murata@gmail.com>
2015-06-26 01:41:49 +0900

    Add unique to User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,14 +27,14 @@ describe User do
   describe "when email formtat is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
-      addresses.each do |valid_address|
-        @user.email = valid_address
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
         expect(@user).not_to be_valid
       end
     end
   end
 
-  describe "when email formtat is invalid" do
+  describe "when email formtat is valid" do
     it "should be invalid" do
       addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
       addresses.each do |valid_address|
@@ -42,6 +42,16 @@ describe User do
         expect(@user).to be_valid
       end
     end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
   end
 
 end
